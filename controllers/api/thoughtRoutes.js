d743e8ca680a506a46050ed847f546c4c4054f13
carterfm <carterf.morfitt@gmail.com>
2022-02-24 17:56:47 -0800

    Set up basic file structure for project

---
+++ controllers/api/thoughtRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { User, Thought } = require('../../models');
+// TODO: insert routes here
+
+
+
+module.exports = router;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { User, Thought } = require('../../models');
+//TODO: routes will go here
+
+
+
+module.exports = router;
