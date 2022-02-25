d736b5e1be5c0eaa87fdeafea9e56933abc598c8
carterfm <carterf.morfitt@gmail.com>
2022-02-24 19:27:24 -0800

    Set up models

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,64 @@
-const {Schema, Types} = require('mongoose');
+const {Schema, Types, model} = require('mongoose');
+const moment = require('moment');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            minLength: 1
+        }, 
+        username: {
+            type: String,
+            required: true
+        }, 
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (created) => moment(created).format("MMMM Do YYYY, h:mm:ss a")
+        }
+
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            minLength: 1
+        }, 
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (created) => moment(created).format("MMMM Do YYYY, h:mm:ss a")
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+
+    }, 
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+//Initializing Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,52 @@
-const {Schema, Types} = require('mongoose');
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            //trims input string by default--so, " user " -> "user", for instance
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                //Writing a custom validator to ensure that we're receiving something in email format
+                //Using the regex expression I examined in the previous HW assignment to verify 
+                validator: function(value) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value);
+                }
+            },
+            thoughts: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'thought'
+                }
+            ],
+            friends: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'user'
+                }
+            ]
+        },
+
+    }, 
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+//Initializing our User model
+const User = model('user', userSchema);
+
+module.exports = User;
